78c197d3222ec36386f5ecbc8fd0638cd1cc8f07
Pratyusha Jayavarapu <Pjayavarapu3016@conestogac.on.ca>
2024-02-12 19:09:12 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunitdemo
+{
+    // PROG8041-PRATYUSHA JAYAVARAPU-8973016-LAB Mutation Testing lab 3
+        namespace NUnitDemonstration
+    {
+        public static class Triangle
+        {
+            public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle) 
+            
+            {
+                string result;
+                if ((firstAngle + secondAngle + thirdAngle) == 180)// killed the mutant
+                {
+                    result = "The triangle is valid";
+                }
+                else
+                {
+                    result = "The triangle is NOT valid";
+                }
+                return result;
+            }
+        }
+    }
+}
+
