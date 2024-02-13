6540d42349241c2b63d7c51c1ccabacf7d262e5c
Pratyusha Jayavarapu <Pjayavarapu3016@conestogac.on.ca>
2024-02-12 19:50:18 -0500

    comment

---
+++ Testclass1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nunitdemo;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Nunitdemo.NUnitDemonstration;
+
+namespace Testclass1
+{
+    public class TriangleTest
+    {
+        // PROG8041-PRATYUSHA JAYAVARAPU-8973016-LAB 2
+        [TestFixture]
+        public class TriangleTest2
+        {
+            [Test]
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid";
+                //Act 
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);
+
+
+
+            }
+        }
+
+    }
+    }
+
+    
+
